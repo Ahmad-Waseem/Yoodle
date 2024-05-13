1712fada83992d239c6f3082e9df8886419712e8
Ahmad-Waseem <eagle24623@gmail.com>
2024-05-13 20:51:55 +0500

    Update to deploy www
    main function init

--- yoodle/yoodle_ui.py
+++ yoodle/yoodle_ui.py
@@ -5,161 +5,164 @@ import time
 import requests
 import json
 
-import pandas as pd
-from PIL import Image
 
-st.set_page_config(page_title="Yoodle - AI Buildings classifier", page_icon="🎨", layout="wide")
+def main():
 
-page_bg_img = '''
+    st.set_page_config(page_title="Yoodle - AI Buildings classifier", page_icon="🎨", layout="wide")
 
-<style>
-[data-testid="stAppViewBlockContainer"]
-    {
-        background-image: url("https://raw.githubusercontent.com/Ahmad-Waseem/Yoodle/main/yoodle/Assets/toppng.com-jpg-black-and-white-istanbul-royalty-colorful-buildings-drawings-3001x1957.png");
-        background-size: cover;
-        padding-top: 1;
-    }
-</style>
-'''
+    page_bg_img = '''
 
-
-# Sidebar
-st.sidebar.title("Yoodle V2")
-st.sidebar.markdown(
-    """
-    ## Introduction
-    Welcome to the Yoodle App! This app uses an AI model to classify buildings based on their features.
-
-    ## Model Description
-    Our model is trained on a large dataset of Augmented  images. It leverages deep learning techniques to predict the Famous Landmarks.
-
-    ## Parameters
-    - **Image Input**: Draw the doodle yourself!!.
-
-    ## How to Use
-    1. Select the Landmark name from DropDown
-    2. Draw doodle under 20 second
-    3. Click the "Predict" button to get the building classification.
-    4. See if model gives you correct image
-
-    Enjoy exploring Yoodle!
-    """
-)
+    <style>
+    [data-testid="stAppViewBlockContainer"]
+        {
+            background-image: url("https://raw.githubusercontent.com/Ahmad-Waseem/Yoodle/main/yoodle/Assets/toppng.com-jpg-black-and-white-istanbul-royalty-colorful-buildings-drawings-3001x1957.png");
+            background-size: cover;
+            padding-top: 1;
+        }
+    </style>
+    '''
 
 
-st.markdown(page_bg_img, unsafe_allow_html=True)
+    # Sidebar
+    st.sidebar.title("Yoodle V2")
+    st.sidebar.markdown(
+        """
+        ## Introduction
+        Welcome to the Yoodle App! This app uses an AI model to classify buildings based on their features.
 
-st.title("Yoodle!!")
-# Dropdown menu for building selection
-lockBoard = False
-submit_button = None
-N = 20
-current_N = N
-YoodleV3 = yoodle_backend.MyModel()
+        ## Model Description
+        Our model is trained on a large dataset of Augmented  images. It leverages deep learning techniques to predict the Famous Landmarks.
+
+        ## Parameters
+        - **Image Input**: Draw the doodle yourself!!.
+
+        ## How to Use
+        1. Select the Landmark name from DropDown
+        2. Draw doodle under 20 second
+        3. Click the "Predict" button to get the building classification.
+        4. See if model gives you correct image
+
+        Enjoy exploring Yoodle!
+        """
+    )
 
 
+    st.markdown(page_bg_img, unsafe_allow_html=True)
 
-
-def printOutput(strings):
-    st.divider()
-    st.write(f"It is: {strings}")
-    st.divider()
-
-
-building = st.selectbox('Select Building', ['None', 'Building A', 'Building B', 'Building C'])
-
-
-if building is None or building == 'None':
-    lockBoard = True
+    st.title("Yoodle!!")
+    # Dropdown menu for building selection
     lockBoard = False
     submit_button = None
     N = 20
     current_N = N
-else:
-    lockBoard = False
+    YoodleV3 = yoodle_backend.MyModel()
 
-    TimerStates = [False, True]
-    isTimerOn = TimerStates[0]
 
-    if not lockBoard:
 
-        # Layout using columns
-        col1, col2 = st.columns(2)
 
-        with col1:
-           
-            drawing_mode = "freedraw"
-            stroke_width = 5
-            stroke_color = "rgb(0, 0, 0)"
-            bg_color = "rgb(255, 255, 255)"
-            realtime_update = True
+    def printOutput(strings):
+        st.divider()
+        st.write(f"It is: {strings}")
+        st.divider()
 
-            # Create a canvas component
-            canvas_result = st_canvas(
-                fill_color="rgb(255, 255, 255)",
-                stroke_width=stroke_width,
-                stroke_color=stroke_color,
-                background_color=bg_color,
-                background_image=None,
-                update_streamlit=realtime_update,
-                height=512,
-                width=512,
-                drawing_mode=drawing_mode,
-                point_display_radius=0 if drawing_mode == 'point' else 0,
-                display_toolbar=False
-            )
 
-        with col2:
-            ph = st.empty()
-            session_state = st.session_state
-            if 'current_N' not in session_state:
-                session_state.current_N = N
+    building = st.selectbox('Select Building', ['None', 'Building A', 'Building B', 'Building C'])
 
-            # Do something interesting with the image data and paths
-            if canvas_result.image_data is not None and canvas_result is not None:
-                isTimerOn = TimerStates[1]
-                
-                
+
+    if building is None or building == 'None':
+        lockBoard = True
+        lockBoard = False
+        submit_button = None
+        N = 20
+        current_N = N
+    else:
+        lockBoard = False
+
+        TimerStates = [False, True]
+        isTimerOn = TimerStates[0]
+
+        if not lockBoard:
+
+            # Layout using columns
+            col1, col2 = st.columns(2)
+
+            with col1:
             
+                drawing_mode = "freedraw"
+                stroke_width = 5
+                stroke_color = "rgb(0, 0, 0)"
+                bg_color = "rgb(255, 255, 255)"
+                realtime_update = True
 
-                if submit_button is None and (20 - session_state.current_N) >= 5:
-                    submit_button = st.button('Submit')
-                    print(session_state.current_N)
-                # Send drawing data to the Flask server for prediction
-                if submit_button:
-                    building = 'None'
-                    drawing_data = canvas_result.image_data
-                    response = YoodleV3.classify(drawing_data, building)
+                # Create a canvas component
+                canvas_result = st_canvas(
+                    fill_color="rgb(255, 255, 255)",
+                    stroke_width=stroke_width,
+                    stroke_color=stroke_color,
+                    background_color=bg_color,
+                    background_image=None,
+                    update_streamlit=realtime_update,
+                    height=512,
+                    width=512,
+                    drawing_mode=drawing_mode,
+                    point_display_radius=0 if drawing_mode == 'point' else 0,
+                    display_toolbar=False
+                )
 
-                    st.write("Drawing submitted!")
-                    printOutput(response)
-                    st.image(canvas_result.image_data)
+            with col2:
+                ph = st.empty()
+                session_state = st.session_state
+                if 'current_N' not in session_state:
+                    session_state.current_N = N
 
-                if isTimerOn:
-                    for secs in range(session_state.current_N, -1, -1):
-                        ss = secs
-                        session_state.current_N = ss
-                        ph.metric("Countdown", f"{ss:02d}s left")
-                        time.sleep(1)
-                    prediction = None
-                    if session_state.current_N <= 1:
-                        isTimerOn = TimerStates[0]
-                        LockBoard = True
-                        print("Locked")
+                # Do something interesting with the image data and paths
+                if canvas_result.image_data is not None and canvas_result is not None:
+                    isTimerOn = TimerStates[1]
+                    
+                    
+                
+
+                    if submit_button is None and (20 - session_state.current_N) >= 5:
+                        submit_button = st.button('Submit')
+                        print(session_state.current_N)
+                    # Send drawing data to the Flask server for prediction
+                    if submit_button:
+                        building = 'None'
+                        drawing_data = canvas_result.image_data
+                        response = YoodleV3.classify(drawing_data, building)
+
+                        st.write("Drawing submitted!")
+                        printOutput(response)
+                        st.image(canvas_result.image_data)
+
+                    if isTimerOn:
+                        for secs in range(session_state.current_N, -1, -1):
+                            ss = secs
+                            session_state.current_N = ss
+                            ph.metric("Countdown", f"{ss:02d}s left")
+                            time.sleep(1)
+                        prediction = None
+                        if session_state.current_N <= 1:
+                            isTimerOn = TimerStates[0]
+                            LockBoard = True
+                            print("Locked")
+
+                            drawing_data = canvas_result.image_data
+                            response = YoodleV3.classify(drawing_data, building)
+                            printOutput(response)
+
+                    elif session_state.current_N % 5 == 0:
+                        isTimerOn = TimerStates[1]
+                        print(f"np{N},{isTimerOn}")
 
                         drawing_data = canvas_result.image_data
                         response = YoodleV3.classify(drawing_data, building)
                         printOutput(response)
 
-                elif session_state.current_N % 5 == 0:
-                    isTimerOn = TimerStates[1]
-                    print(f"np{N},{isTimerOn}")
-
-                    drawing_data = canvas_result.image_data
-                    response = YoodleV3.classify(drawing_data, building)
-                    printOutput(response)
-
-    else:
-        building = 'None'
+        else:
+            building = 'None'
 
 
+
+if __name__ == "__main__":
+  main()
